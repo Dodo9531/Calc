befa52f0a9fffda8873138beb10c208d8fa3138a
Dodo9531 <oge2782@mail.ru>
2023-08-28 21:08:06 +0300

    Calc

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,41 @@ public class Main {
     public Main() {
     }
 
-    public static void String_calc(String input) {
+    public static void String_calc(String input) // Метод для расчётов
+    {
         boolean first_num_is_rome = false;
-        boolean second_num_is_rome = false;
+        boolean second_num_is_rome = false; // Показатели того были ли введены римские цифры или арабские
         String Rome_number;
 
         int first_num;
         int second_num;
 
         try {
-            first_num = Integer.parseInt(input.substring(0, input.indexOf(32)));
-        } catch (NumberFormatException var10) {
-            Rome_number = var10.getMessage().substring(var10.getMessage().indexOf(34) + 1, var10.getMessage().lastIndexOf(34));
+            first_num = Integer.parseInt(input.substring(0, input.indexOf(' ')));
+        } catch (NumberFormatException e) // Если вылезло исключение формата чисел значит введена строка которая преобразуется в римское число
+        {
+            Rome_number = e.getMessage().substring(e.getMessage().indexOf('"') + 1, e.getMessage().lastIndexOf('"'));
             first_num = RomeToArabic(Rome_number);
             first_num_is_rome = true;
         }
 
-        if (first_num > 10) {
+        if (first_num > 10) // Проверка не выходит ли число за заданные рамки
+        {
             throw new NumberValuesException("First number is greater than 10");
         } else if (first_num < 1) {
             throw new NumberValuesException("First number is less than 1");
         } else {
 
-            try {
-                second_num = Integer.parseInt(input.substring(input.lastIndexOf(32) + 1));
-            } catch (NumberFormatException var9) {
-                Rome_number = var9.getMessage().substring(var9.getMessage().indexOf(34) + 1, var9.getMessage().lastIndexOf(34));
+            try // Повторяются все теже действия но для второго числа
+            {
+                second_num = Integer.parseInt(input.substring(input.lastIndexOf(' ') + 1));
+            } catch (NumberFormatException e) {
+                Rome_number = e.getMessage().substring(e.getMessage().indexOf('"') + 1, e.getMessage().lastIndexOf('"'));
+                if (Rome_number.isEmpty() || Rome_number.equals("+") || Rome_number.equals("/") || Rome_number.equals("-") || Rome_number.equals("*"))
+                    throw new ExpressionFormatException("Expression isn't correct");
                 second_num = RomeToArabic(Rome_number);
                 second_num_is_rome = true;
+
             }
 
             if (first_num_is_rome != second_num_is_rome) {
@@ -44,9 +51,10 @@ public class Main {
             } else if (second_num < 1) {
                 throw new NumberValuesException("Second number is less than 1");
             } else {
-                String operation = input.substring(input.indexOf(32) + 1, input.lastIndexOf(32));
+                String operation = input.substring(input.indexOf(' ') + 1, input.lastIndexOf(' ')); // Смотрим какая операция
                 int out_num = -1;
-                switch (operation) {
+                switch (operation) //Выполнение операции
+                {
                     case "+":
                         out_num = first_num + second_num;
                         break;
@@ -60,15 +68,15 @@ public class Main {
                         out_num = first_num / second_num;
                 }
 
-                if (first_num_is_rome && second_num_is_rome) {
-                    if (out_num < 1) {
+                if (first_num_is_rome && second_num_is_rome) // Проверка на то римские ли числа даны на старте
+                {
+                    if (out_num < 1) // Проверка результата
                         throw new NumberValuesException("Result of rome expression can't be 0 or less");
-                    }
 
-                    System.out.println(ArabicToRome(out_num));
-                } else {
-                    System.out.println(out_num);
-                }
+                    System.out.println(ArabicToRome(out_num)); //Вывод римского числа
+                } else
+                    System.out.println(out_num); // Вывод арабского числа
+
 
             }
         }
@@ -97,61 +105,57 @@ public class Main {
         }
 
         return Arabic_number;
-    }
+    } // Перевод в римские с арабских
 
     private static String ArabicToRome(int Arabic_number) {
         String Rome_number = "";
 
         while(Arabic_number > 0) {
             if (Arabic_number == 100) {
-                Rome_number = Rome_number + "C";
+                Rome_number += "C";
                 Arabic_number -= 100;
             }
 
             while(Arabic_number >= 50) {
-                Rome_number = Rome_number + "L";
+                Rome_number +=  "L";
                 Arabic_number -= 50;
             }
 
             while(Arabic_number >= 10) {
-                Rome_number = Rome_number + "X";
+                Rome_number +=  "X";
                 Arabic_number -= 10;
             }
 
             while(Arabic_number >= 5) {
-                Rome_number = Rome_number + "V";
+                Rome_number += "V";
                 Arabic_number -= 5;
             }
 
             while(Arabic_number > 0 && Arabic_number < 5) {
-                Rome_number = Rome_number + "I";
+                Rome_number += "I";
                 --Arabic_number;
             }
         }
 
         return Rome_number;
-    }
+    }  // Перевод арабских чисел в римские
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String expression = sc.nextLine();
         int spaces = -2;
         String expcheck = expression;
-        if (expression.indexOf(32) == -1) {
+        if (expcheck.indexOf(' ') == -1) {
             throw new ExpressionFormatException("It's not expression");
-        } else {
-            do {
-                if (expcheck.indexOf(32) == -1) {
-                    String_calc(expression);
-                    return;
-                }
-
-                expcheck = expcheck.substring(0, expcheck.lastIndexOf(32));
-                ++spaces;
-            } while(spaces <= 0);
-
-            throw new ExpressionFormatException("Expression isn't correct");
-        }
+        } else
+            while(expcheck.indexOf(' ') != -1){
+                    expcheck = expcheck.substring(0, expcheck.lastIndexOf(' '));
+                    spaces++;
+                if(spaces>0)
+                    throw new ExpressionFormatException("Expression isn't correct");
+            }
+            if(spaces==-1)
+                throw new ExpressionFormatException("Expression isn't correct");
+        String_calc(expression);
     }
-
 }
